8915df53f78f9ae0efc43e88174123c1b362962f
Peter <neetisthebest@gmail.com>
2014-12-27 11:25:48 -0800

    finished first project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-	validates :name, presence: true#, length: {maximum:50}
-#	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true#, length: {maximum:255}
-	validates :password, presence: true # length: {minimun:6}
-	end
+	validates :username, presence: true
+	validates :email, presence: true
+	validates :password, presence: true 
+	
 end
